ee06aeaedcb4fd7f5aa54827ed871f2d93a49750
Nishizaki <steelpipe75@gmail.com>
2014-05-07 22:17:30 +0900

    yamlからjsonに変換

---
+++ lib/yaml/yaml2json/yaml2json.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby -Ku
+
+require 'pp'
+require 'json'
+require 'yaml'
+
+unless ARGV.size == 2 then
+  puts "Usage: yaml2json in_file out_file"
+  exit 1
+end
+
+data = YAML.load_file(ARGV[0])
+File.open(ARGV[1],'w'){ |f|
+  f.write data.to_json
+}
