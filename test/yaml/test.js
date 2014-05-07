4d1248a5243340d08321a8131b48a56b563b1f45
Nishizaki <steelpipe75@gmail.com>
2014-05-07 22:16:26 +0900

    yamlライブラリ削除

--- test/yaml/test.js
+++
@@ -1,9 +0,0 @@
-human = {};
-human.lastname = "fukuda";
-human.firstname = "akinobu";
-
-WScript.Echo( WSH_YAML.stringify(human) );
-
-test = WSH_YAML.parse('{hoge:"foo"}');
-
-WScript.Echo( test.hoge );
