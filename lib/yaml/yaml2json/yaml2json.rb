e39403d5bc8a20d1a27c2bc7f9cb47efbfbae193
Nishizaki <steelpipe75@gmail.com>
2014-05-28 22:05:10 +0900

    tab変換してからjsonに変換

--- lib/yaml/yaml2json/yaml2json.rb
+++ lib/yaml/yaml2json/yaml2json.rb
@@ -4,12 +4,24 @@ require 'pp'
 require 'json'
 require 'yaml'
 
-unless ARGV.size == 2 then
-  puts "Usage: yaml2json in_file out_file"
+unless (ARGV.size == 2) || (ARGV.size == 3 && ARGV[2] == "tab") then
+  puts "Usage: yaml2json in_file out_file [tab]"
   exit 1
 end
 
-data = YAML.load_file(ARGV[0])
+txt = File.read(ARGV[0])
+if ARGV[2] == "tab" then
+  yaml = ""
+  txt.each_line do |line|
+    while /\t+/ =~ line
+      n = $&.size * 2 - $`.size % 2
+      line.sub!(/\t+/, " " * n)
+    end
+    yaml << line
+  end
+  txt = yaml
+end
+data = YAML.load(txt)
 File.open(ARGV[1],'w'){ |f|
   f.write JSON.pretty_generate(data).encode("Shift_JIS")
 }
